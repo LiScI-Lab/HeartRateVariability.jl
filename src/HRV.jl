95ddcd9aafa2b86e8d41fbca9ef14cdd91c474dd
Jasmin Walter <jasmin.walter@mni.thm.de>
2020-04-08 02:28:47 +0200

    time_domain functions defined

--- src/HRV.jl
+++ src/HRV.jl
@@ -2,8 +2,20 @@ module HRV
 
 include("TimeDomain.jl")
 
-function time_domain(n)
-    print(SDNN(n))
+function time_domain(n::Array,s::String="all")
+    diff=TimeDomain.nn_diff(n)
+    println("Mean: ",TimeDomain.mean_nn(n))
+    println("SDNN: ",TimeDomain.sdnn(n))
+    println("RMSSD: ",TimeDomain.rmssd(diff))
+    println("SDSD: ",TimeDomain.sdsd(diff))
+    println("NN50: ",TimeDomain.nn(diff,50))
+    println("pNN50: ",TimeDomain.pnn(diff,50))
+    println("NN20: ",TimeDomain.nn(diff,20))
+    println("pNN20: ",TimeDomain.pnn(diff,20))
+end
+
+function time_domain(n::Array,s::Array)
+    print("test")
 end
 
 end # module
--- src/TimeDomain.jl
+++ src/TimeDomain.jl
@@ -1,5 +1,47 @@
-using Statistics
+module TimeDomain
+    using Statistics
 
-function SDNN(n)
-    return std(n)
+    function nn_diff(n)
+        diff=[]
+        i=1
+        while i<length(n)
+            push!(diff,abs(n[i+1]-n[i]))
+            i+=1
+        end
+        return diff
+    end
+
+    function sdnn(n)
+        return std(n)
+    end
+
+    function sdann(n)
+    end
+
+    function rmssd(diff)
+        return sqrt(mean(diff.^2))
+    end
+
+    function sdsd(diff)
+        return std(diff)
+    end
+
+    function pnn(diff,x)
+        return nn(diff,x)/length(diff)
+    end
+
+    function nn(diff,x)
+        count=0
+        for d in diff
+            if d>x
+                count+=1
+            end
+        end
+        return count
+    end
+
+    function mean_nn(n)
+        return mean(n)
+    end
+
 end
