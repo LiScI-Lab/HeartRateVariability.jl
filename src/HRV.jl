35d72e28f7c537eb13f66a93714fd15104378fbf
Jasmin Walter <jasmin.walter@mni.thm.de>
2020-05-18 22:49:04 +0200

    updated documentation

--- src/HRV.jl
+++ src/HRV.jl
@@ -5,12 +5,14 @@ include("Input.jl")
 include("Frequency.jl")
 include("Nonlinear.jl")
 
-#=
+"""
 This function calculates the nonlinear analysis methods and prints the results
-:param n: is the array that contains the NN-inetrvals
-:param m: is the embedding dimension, default=2
-:param r: is the tolerance, default=6
-=#
+
+Arguments:
+- n : is the array that contains the NN-inetrvals
+- m : is the embedding dimension, default=2
+- r : is the tolerance, default=6
+"""
 function nonlinear(n::Array{Float64,1},m::Int64=2,r::Number=6)
     println("ApEn: ",Nonlinear.apen(n,m,r))
     println("SampEn: ",Nonlinear.sampen(n,m,r))
@@ -19,10 +21,10 @@ function nonlinear(n::Array{Float64,1},m::Int64=2,r::Number=6)
     println("Renyi 2: ",Nonlinear.renyi(n,2))
 end # nonlinear
 
-#=
+"""
 This function calculates the frequency analysis methods and prints the results
 :param n: is the array that contains the NN-inetrvals
-=#
+"""
 function frequency(n::Array{Float64,1})
     ls=Frequency.lomb_scargle(n)
     vlf=Frequency.get_power(ls.freq,ls.power,0.003,0.04)
@@ -36,10 +38,10 @@ function frequency(n::Array{Float64,1})
     println("Total Power: ",tp)
 end # frequency
 
-#=
+"""
 This function calculates the time domain analysis methods and prints the results
 :param n: is the array that contains the NN-inetrvals
-=#
+"""
 function time_domain(n::Array{Float64,1})
     diff=TimeDomain.nn_diff(n)
     println("Mean: ",TimeDomain.mean_nn(n))
@@ -52,23 +54,24 @@ function time_domain(n::Array{Float64,1})
     println("pNN20: ",TimeDomain.pnn(diff,20))
 end # time_domain
 
-#=
+"""
 This function reads a file (txt or csv) and saves the data in an array
 :param file: is the location of the input file
 :return: an array containing the NN intervals
-=#
+"""
 function infile(file::String)
     return Input.read_txt(file)
 end # infile
 
-#=
+"""
 This function reads a wfdb file and saves the data in an array
 :param record: is the name of the record
 :param annotator: is the annotator of the record
 :return: an array containing the NN intervals
-=#
+"""
 function infile(record::String,annotator::String)
     return Input.read_wfdb(record,annotator)
 end # infile
 
+export nonlinear, frequency, time_domain, infile
 end # module
