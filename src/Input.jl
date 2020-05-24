c0e8752ae58ccbc12e3077f512c895cc3441d49a
Jasmin Walter <jasmin.walter@mni.thm.de>
2020-05-25 00:18:43 +0200

    remove temporary text file

--- src/Input.jl
+++ src/Input.jl
@@ -8,7 +8,7 @@ end
 function read_wfdb(record::String,annotator::String)
     run(pipeline(`ann2rr -r "$record" -a "$annotator" -i s -c`,stdout="temp_rr.txt"))
     a=read_txt("temp_rr.txt")
-    #run(`rm "temp_rr.txt"`)
+    run(`rm "temp_rr.txt"`)
     return a*1000
 end
 
