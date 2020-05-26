341e809c2a44f1d532c47628ba75c4a405e6e30b
Jasmin Walter <jasmin.walter@mni.thm.de>
2020-05-26 04:12:15 +0200

    tests for frequency and nonlinear added

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,8 @@ using Test
 
 n=HRV.infile("e1304.txt")
 td=HRV.time_domain(n)
-#fd=HRV.frequency(n)
-#nl=HRV.nonlinear(n)
+fd=HRV.frequency(n)
+nl=HRV.nonlinear(n)
 
 @testset "HRV.jl" begin
 
@@ -21,5 +21,22 @@ td=HRV.time_domain(n)
         @test td.pnn50≈4.413472706155633 atol=0.1
         @test td.nn20≈2831 atol=1
         @test td.pnn20≈36.53374628984385 atol=0.1
+    end
+
+    @testset "HRV.frequency" begin
+        @test fd.vlf≈1317.96 atol=0.01*fd.vlf
+        @test fd.lf≈90.36 atol=0.01*fd.lf
+        @test fd.hf≈176.05 atol=0.01*fd.hf
+        @test fd.lf_hf_ratio≈0.51 atol=0.01*fd.lf_hf_ratio
+        @test fd.tp≈1584.35 atol=0.01*fd.tp
+    end
+
+    @testset "HRV.nonlinear" begin
+        @test nl.apen≈2.16 atol=0.1
+        @test nl.sampen≈2.16 atol=0.1
+        @test nl.hurst≈0.37 atol=0.1
+        @test nl.renyi0≈-6.82 atol=0.1
+        @test nl.renyi1≈-6.83 atol=0.1
+        @test nl.renyi2≈-6.84 atol=0.1
     end
 end
