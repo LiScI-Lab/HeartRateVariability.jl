062d00871e880d91a67a4ad06f8a3151b94f5ff6
Jasmin Walter <jasmin.walter@mni.thm.de>
2020-06-01 22:28:19 +0200

    documentation updated

--- docs/make.jl
+++ docs/make.jl
@@ -14,10 +14,8 @@ makedocs(modules=[HRV],
                   #  "License" => "LICENSE.md"
                   ],
                   # Use clean URLs, unless built as a "local" build
-          html_prettyurls = !("local" in ARGS),
-          html_canonical = "https://LiScI-Lab.github.io/HRV.jl/stable/",
-         )
-         
+          )
+
 deploydocs(
     repo = "github.com/LiScI-Lab/HRV.jl.git",
 )
