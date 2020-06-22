b28715b0acc21910e2b8ac937588c3da1c395451
Jasmin Walter <jasmin.walter@mni.thm.de>
2020-06-22 21:44:36 +0200

    documentation added

--- src/Nonlinear.jl
+++ src/Nonlinear.jl
@@ -3,26 +3,53 @@ module Nonlinear
 import StatsBase
 import Statistics
 
+#=
+This function calculates the approximate entropy
+:param n: the array that contains the NN-inetrvals
+:param m: the embedding dimension, default=2
+:param r: the tolerance, default=6
+:return: the approximate entropy
+=#
 function apen(n,m,r)
     c1=get_apen_dist(n,m,r)
     c2=get_apen_dist(n,m+1,r)
     return log(c1/c2)
-end
+end # apen
 
+#=
+This function calculates the sample entropy
+:param n: the array that contains the NN-inetrvals
+:param m: the embedding dimension, default=2
+:param r: the tolerance, default=6
+:return: the sample entropy
+=#
 function sampen(n,m,r)
     c1=get_sampen_dist(n,m,r,1)
     c2=get_sampen_dist(n,m+1,r,0)
     return -log(c2/c1)
-end
+end # sampen
 
+#=
+This function creates a template of a given array over an embedding dimension
+:param n: the array that contains the NN-inetrvals
+:param m: the embedding dimension, default=2
+:return template: the created template
+=#
 function get_template(n,m)
     template=[]
     for i in 1:length(n)-m+1
         push!(template,n[i:i+m-1])
     end
     return template
-end
+end # get_template
 
+#=
+This function calculates the distances for the approximate entropy
+:param n: the array that contains the NN-inetrvals
+:param m: the embedding dimension, default=2
+:param r: the tolerance, default=6
+:return: the distance for the approximate entropy
+=#
 function get_apen_dist(n,m,r)
     template=get_template(n,m)
     count=zeros(length(template))
@@ -35,8 +62,16 @@ function get_apen_dist(n,m,r)
         end
     end
     return sum(count./(length(n)-m+1))/(length(n)-m+1)
-end
+end # get_apen_dist
 
+#=
+This function calculates the distances for the sample entropy
+:param n: the array that contains the NN-inetrvals
+:param m: the embedding dimension, default=2
+:param r: the tolerance, default=6
+:param l: a value to limit the for-loops
+:return: the distance for the sample entropy
+=#
 function get_sampen_dist(n,m,r,l)
     template=get_template(n,m)
     counts=[]
@@ -52,12 +87,24 @@ function get_sampen_dist(n,m,r,l)
         end
     end
     return sum(counts)
-end
+end # get_sampen_dist
 
+#=
+This function calculates the renyi entropy of a given order
+:param n: the array that contains the NN-inetrvals
+:param a: the order of the renyi entropy
+:return: the calculated renyi entropy
+=#
 function renyi(n,a)
     return StatsBase.renyientropy(n,a)
 end # renyi
 
+#=
+This function calculates the hurst coefficient
+It was inspired by the python hurst package by Dmitry A. Mottl (https://github.com/Mottl/hurst)
+:param n: the array that contains the NN-inetrvals
+:return H: the hurst coefficient
+=#
 function hurst(n)
     ws=Array(range(log10(10),stop=log10(length(n)),step=0.25))
     window=[]
@@ -93,8 +140,14 @@ function hurst(n)
     H,c=A\B
     c=exp10(c)
     return H
-end
+end # hurst
 
+#=
+This function calculates the rescaled range of a time series
+It was inspired by the python hurst package by Dmitry A. Mottl (https://github.com/Mottl/hurst)
+:param n: the array that contains the NN-inetrvals
+:return: the rescaled range
+=#
 function get_rs(n)
     incs=n[2:end].-n[1:end-1]
     mean_inc=(n[end]-n[1])/length(incs)
@@ -107,6 +160,6 @@ function get_rs(n)
     else
         return R/S
     end
-end
+end # get_rs
 
 end # module
