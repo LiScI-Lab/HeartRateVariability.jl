d77e7d7008a3ffae1bd9d31107032891e1b5fd2f
Jasmin Walter <jasmin.walter@mni.thm.de>
2020-06-22 21:19:54 +0200

    documentation added

--- src/Frequency.jl
+++ src/Frequency.jl
@@ -15,7 +15,14 @@ function lomb_scargle(n)
     return LombScargle.lombscargle(plan)
 end # lomb_scargle
 
-
+#=
+This function calculates the power of a frequency band between two given frequencys
+:param freq: The frequency of a lomb scargle transformation
+:param power: The power of a lomb scargle transformation
+:param min: The minimum value of the frequency band to be calculated
+:param max: The maximum value of the frequency band to be calculated
+:return p: The power of the frequency band
+=#
 function get_power(freq,power,min,max)
     count=1
     index=[]
