7b66e2043be088895f9872009799c5be624d6b91
Jasmin Walter <jasmin.walter@mni.thm.de>
2020-06-22 21:31:03 +0200

    documentation added; renamed temp_file

--- src/Input.jl
+++ src/Input.jl
@@ -1,15 +1,27 @@
 module Input
 
+#=
+This function reads the data from a txt or csv file.
+:param infile: the path of the file
+:return: an array with the read data
+=#
 function read_txt(infile::String)
     a=read(open(infile,"r"),String)
     return parse.(Float64,filter!(e->e!="",split(a,r"[^\d.]")))
-end
+end # infile
 
+#=
+This function reads the data from a wfdb file.
+:param record: the record name
+:param annotator: the annotator of the record
+:return: an array with the read data
+=#
 function read_wfdb(record::String,annotator::String)
-    run(pipeline(`ann2rr -r "$record" -a "$annotator" -i s -c`,stdout="temp_rr.txt"))
-    a=read_txt("temp_rr.txt")
-    run(`rm "temp_rr.txt"`)
+    temp=string(record,"_temp.txt")
+    run(pipeline(`ann2rr -r "$record" -a "$annotator" -i s -c`,stdout="$temp"))
+    a=read_txt("$temp")
+    run(`rm "$temp"`)
     return a*1000
-end
+end # infile
 
-end
+end # module
